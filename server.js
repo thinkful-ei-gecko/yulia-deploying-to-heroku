962d05f369ccade647efbe3d3f074de109ff2419
Yulia Khisamutdinova <yulia-kh@users.noreply.github.com>
2019-09-27 12:33:40 -0700

    prepare for deployment

---
+++ server.js
@@ -0,0 +1,64 @@
+'use strict';
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const movies = require('./moviestore.json');
+
+const app = express();
+
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
+app.use(helmet());
+app.use(cors());
+
+//function for validation
+app.use(function validateBearerToken(req, res, next){
+  const apiToken = process.env.API_TOKEN;
+  const authToken = req.get('Authorization');
+  if(!authToken || authToken.split(' ')[1] !== apiToken){
+    return res.status(401).json({ error: 'Unauthorized request made'})
+  }
+  next();
+});
+
+//function for requesting the movies by user query
+function handleGetMovies(req, res) {
+  const { genre, country, avg_vote} = req.query;
+  let result = movies;
+
+  if (genre ) {
+    result = result.filter( movie =>
+      movie.genre.toLowerCase().includes(genre.toLowerCase())
+    );
+  }
+  if (country) {
+    result = result.filter( movie => 
+      movie.country.toLowerCase().includes(country.toLowerCase())
+    );
+  }
+  if (avg_vote) {
+    result = result.filter( movie => {
+      const avg = parseFloat(avg_vote);
+      return parseFloat(movie.avg_vote) >= avg;
+    });
+  }
+  return res.status(200).json(result);
+}
+
+app.get('/movies', handleGetMovies);
+
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }}
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
+
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT);
